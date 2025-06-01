d38a3f4157b1e283d877939388bce5466ae49d51
Nicholas Griffiths <nichg999@gmail.com>
2025-05-31 22:23:14 -0600

    completed algorithm 4  with codewars

--- index.js
+++ index.js
@@ -48,3 +48,11 @@ function likes(names) {
 function findOdd(A) {
   return A.reduce((acc, num) => acc ^ num, 0)
 }
+
+// Algo 4
+
+// Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string (alphabetical ascending), the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+function longest (s1, s2) {
+  return Array.from(new Set(s1 + s2)).sort().join('');
+}
