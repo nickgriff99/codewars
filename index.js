7ab6db8ec063774419c815a143d5e54db877b626
Nicholas Griffiths <nichg999@gmail.com>
2025-05-31 21:41:18 -0600

    finished algorithm 3 from codewars

--- index.js
+++ index.js
@@ -33,3 +33,18 @@ function likes(names) {
   if (names.length === 3) return `${names[0]}, ${names[1]} and ${names[2]} like this`;
   return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
 }
+
+// Algo 3
+
+// Given an array of integers, find the one that appears an odd number of times.
+// There will always be only one integer that appears an odd number of times.
+// Examples
+// [7] should return 7, because it occurs 1 time (which is odd).
+// [0] should return 0, because it occurs 1 time (which is odd).
+// [1,1,2] should return 2, because it occurs 1 time (which is odd).
+// [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+// [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+function findOdd(A) {
+  return A.reduce((acc, num) => acc ^ num, 0)
+}
