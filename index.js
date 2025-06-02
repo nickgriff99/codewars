59477201982e7502622573bc0c0d10d210293eff
Nicholas Griffiths <nichg999@gmail.com>
2025-06-02 00:27:54 -0600

    finished algo 15 with codewars

--- index.js
+++ index.js
@@ -183,3 +183,14 @@ function isValidIP(str) {
   return regex.test(str);
 }
 
+// Algo 15
+
+// You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+
+function findOutlier(integers) {
+  const even = integers.filter(num => num % 2 === 0);
+  const odd = integers.filter(num => num % 2 !== 0);
+  return even.length === 1 ? even[0] : odd[0];
+}
+
+console.log(findOutlier([160, 3, 1719, 11, 13, -21]));
