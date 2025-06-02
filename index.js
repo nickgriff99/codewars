ac0803c408c052fd7a586cb7b9504078a8f2a25c
Nicholas Griffiths <nichg999@gmail.com>
2025-06-02 00:17:50 -0600

    completed algo 14 for codewars

--- index.js
+++ index.js
@@ -173,4 +173,13 @@ function duplicateEncode(word) {
   return result;
 }
 
-console.log(duplicateEncode("success"));
+// Algo 14
+
+// Write an algorithm that will identify valid IPv4 addresses in dot-decimal format. IPs should be considered valid if they consist of four octets, with values between 0 and 255, inclusive.
+
+function isValidIP(str) {
+  const octet = '(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?|0)';
+  const regex = new RegExp(`^${octet}\\.${octet}\\.${octet}\\.${octet}$`);
+  return regex.test(str);
+}
+
