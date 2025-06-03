e8858d88334257d7318a9c300c691197c65b6026
Nicholas Griffiths <nichg999@gmail.com>
2025-06-02 21:12:04 -0600

    completed algo 19 with codewars

--- index.js
+++ index.js
@@ -255,3 +255,20 @@ function findMissingLetter(array) {
   }
 }
 
+// Algo 19
+
+// You will be given a number and you will need to return it as a string in Expanded Form. For example:
+//    12 --> "10 + 2"
+//    45 --> "40 + 5"
+// 70304 --> "70000 + 300 + 4"
+// NOTE: All numbers will be whole numbers greater than 0.
+
+function expandedForm(num) {
+  return num
+    .toString()
+    .split('')
+    .map((digit, index, arr) => digit * Math.pow(10, arr.length - index - 1))
+    .filter(Boolean)
+    .join(' + ');
+}
+
