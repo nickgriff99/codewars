25395287a7ba3178d5c74fb280a93d8045b74566
Nicholas Griffiths <nichg999@gmail.com>
2025-06-02 21:37:00 -0600

    completed algo 21 with codewars

--- index.js
+++ index.js
@@ -290,3 +290,33 @@ function toWeirdCase(string) {
     .join(' ');
 }
 
+// Algo 21 
+
+// Write a function that takes a string of braces, and determines if the order of the braces is valid. It should return true if the string is valid, and false if it's invalid.
+// This Kata is similar to the Valid Parentheses Kata, but introduces new characters: brackets [], and curly braces {}. Thanks to @arnedag for the idea!
+// All input strings will be nonempty, and will only consist of parentheses, brackets and curly braces: ()[]{}.
+// What is considered Valid?
+// A string of braces is considered valid if all braces are matched with the correct brace.
+// Examples
+// "(){}[]"   =>  True
+// "([{}])"   =>  True
+// "(}"       =>  False
+// "[(])"     =>  False
+// "[({})](]" =>  False
+
+function validBraces(braces) {
+  const stack = [];
+  const open = '([{';
+  const close = ')]}';
+  for (const char of braces) {
+    if (open.includes(char)) {
+      stack.push(char);
+    } else {
+      if (stack.length === 0 || open.indexOf(stack.pop()) !== close.indexOf(char)) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+}
+
