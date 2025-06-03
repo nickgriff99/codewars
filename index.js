46feb78b716e0cec34e1e203073e290f291adb8e
Nicholas Griffiths <nichg999@gmail.com>
2025-06-02 20:39:25 -0600

    completed algo 17 with codewars

--- index.js
+++ index.js
@@ -212,3 +212,23 @@ function narcissistic(value) {
   return sum === value;
 }
 
+// Algo 17
+
+// Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+// For example:
+// uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+// uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+// uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+
+var uniqueInOrder = function(iterable) {
+  const result = [];
+  let lastElement = null;
+  for (const element of iterable) {
+    if (element !== lastElement) {
+      result.push(element);
+      lastElement = element;
+    }
+  }
+  return result;
+}
+
