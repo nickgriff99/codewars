22e7ae340de0ed2cbd476e3afd55c0ea41c6a805
Nicholas Griffiths <nichg999@gmail.com>
2025-06-04 00:13:41 -0600

    completed algo 26 with codewars

--- index.js
+++ index.js
@@ -486,3 +486,35 @@ function findEvenIndex(arr) {
   return -1;
 }
 
+// Algo 26
+
+// A triangle number is a number where n objects form an equilateral triangle (it's a bit hard to explain). For example, 6 is a triangle number because you can arrange 6 objects into an equilateral triangle:
+//   1  
+//  2 3 
+// 4 5 6
+// 8 is not a triangle number because 8 objects do not form an equilateral triangle:
+//    1
+//   2 3
+//  4 5 6
+// 7 8
+// In other words, the nth triangle number is equal to the sum of the n natural numbers from 1 to n.
+// Your task:
+// Check if a given input is a valid triangle number. Return true if it is, false if it is not (note that any non-integers, including non-number types, are not triangle numbers).
+// You are encouraged to develop an effective algorithm: test cases include really big numbers.
+// Assumptions:
+// You may assume that the given input, if it is a number, is always positive.
+// Notes:
+// 0 and 1 are triangle numbers.
+
+function isTriangleNumber(number) {
+  if (typeof number !== 'number' || !Number.isInteger(number) || number < 0) {return false;}
+  if (number === 0 || number === 1) {return true;}
+  let sum = 0;
+  for (let n = 1; sum < number; n++) {
+    sum += n;
+    if (sum === number) {
+      return true;
+    }
+  }
+  return false;
+}
