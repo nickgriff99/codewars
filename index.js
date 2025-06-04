df632bd54aeac19091d9bc74c5cc276f677c72fb
Nicholas Griffiths <nichg999@gmail.com>
2025-06-03 22:17:46 -0600

    finished algo 23 with codewars

--- index.js
+++ index.js
@@ -367,3 +367,54 @@ function solution(number) {
   return str;
 }
 
+// Algo 23
+
+// Create a function that takes a Roman numeral as its argument and returns its value as a numeric decimal integer. You don't need to validate the form of the Roman numeral.
+// Modern Roman numerals are written by expressing each decimal digit of the number to be encoded separately, starting with the leftmost digit and skipping any 0s. So 1990 is rendered "MCMXC" (1000 = M, 900 = CM, 90 = XC) and 2008 is rendered "MMVIII" (2000 = MM, 8 = VIII). The Roman numeral for 1666, "MDCLXVI", uses each letter in descending order.
+// Example:
+// "MM"      -> 2000
+// "MDCLXVI" -> 1666
+// "M"       -> 1000
+// "CD"      ->  400
+// "XC"      ->   90
+// "XL"      ->   40
+// "I"       ->    1
+// Help:
+// Symbol    Value
+// I          1
+// V          5
+// X          10
+// L          50
+// C          100
+// D          500
+// M          1,000
+
+function solution(roman) {
+  var num = {
+    I: 1,
+    IV: 4,
+    V: 5,
+    IX: 9,
+    X: 10,
+    XL: 40,
+    L: 50,
+    XC: 90,
+    C: 100,
+    CD: 400,
+    D: 500,
+    CM: 900,
+    M: 1000,
+  };
+  var total = 0;
+  for (let i = 0; i < roman.length; i++) {
+    const current = num[roman[i]];
+    const next = num[roman[i + 1]];
+    if (next && current < next) {
+      total -= current;
+    } else {
+      total += current;
+    }
+  }
+  return total;
+}
+
