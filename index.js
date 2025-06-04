38335aba1ea74e8bdc26aa2fe8a930db31708c25
Nicholas Griffiths <nichg999@gmail.com>
2025-06-03 20:50:10 -0600

    finished algo 22 with codewars

--- index.js
+++ index.js
@@ -320,3 +320,44 @@ function validBraces(braces) {
   return stack.length === 0;
 }
 
+// Algo 22
+
+// Create a function taking a positive integer between 1 and 3999 (both included) as its parameter and returning a string containing the Roman Numeral representation of that integer.
+// Modern Roman numerals are written by expressing each digit separately starting with the leftmost digit and skipping any digit with a value of zero. There cannot be more than 3 identical symbols in a row.
+// In Roman numerals:
+// 1990 is rendered: 1000=M + 900=CM + 90=XC; resulting in MCMXC.
+// 2008 is written as 2000=MM, 8=VIII; or MMVIII.
+// 1666 uses each Roman symbol in descending order: MDCLXVI.
+// Example:
+//    1 -->       "I"
+// 1000 -->       "M"
+// 1666 --> "MDCLXVI"
+// Help:
+// Symbol    Value
+// I          1
+// V          5
+// X          10
+// L          50
+// C          100
+// D          500
+// M          1,000
+
+function solution(number) {
+  var roman = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+  };
+  var str = '';
+  for (var i of Object.keys(roman)) {
+    var j - Math.floor(number / roman[i]);
+    number -= j * roman[i];
+    str += i.repeat(j);
+  }
+  return str;
+}
+
