f56637f4e81146c8055cedc250e62f3b1b7d7605
Nicholas Griffiths <nichg999@gmail.com>
2025-06-29 18:11:51 -0600

    finished algo 3

--- index2.js
+++ index2.js
@@ -61,3 +61,59 @@ function getMostFrequent(json) {
 function countBits(n) {
   return n.toString(2).split('').filter(bit => bit === '1').length;
 }
+
+// Algo 3
+
+// The principle is pretty simple:
+
+// Given a integer (n)
+// Find the next palindromic number after (excluding) n
+// Implement this in the function nextPalin
+
+// However, due to some constraints, the implementation is not:
+
+// 0 < n < 10^1000
+// 0 < t < 0.175s
+// A.K.A
+// n is between 1 and 1001 digits
+// total time for all test cases should be less than 0.175s (175ms)
+// This means that you cannot do:
+
+// function nextPalin(n) {
+//   while (!isPalin(n)) n += 1n; // isPalin(n) is defined elsewhere
+//   return n;
+// }
+// For example:
+
+// 12345 -> 12421
+// 11 -> 22
+// 134 -> 141
+// 9876543219123456789 -> 9876543220223456789
+// There are 322 test cases (excluding time constraint, which is not included in assertion), so optimization is very important
+
+function nextPalin(number) {
+  number = typeof number === "string" ? number : number.toString();
+  if (/^9+$/.test(number)) {
+    return BigInt("1" + "0".repeat(number.length - 1) + "1");
+  }
+  const len = number.length;
+  const half = Math.floor(len / 2);
+  let left = number.slice(0, half);
+  let middle = len % 2 === 0 ? "" : number[half];
+  function mirror(l, m) {
+    return l + m + l.split('').reverse().join('');
+  }
+  let mirrored = mirror(left, middle);
+  if (mirrored > number) return BigInt(mirrored);
+  let increment = (BigInt(left + middle) + 1n).toString();
+  while (increment.length < left.length + middle.length) increment = "0" + increment;
+  let newLeft, newMiddle;
+  if (middle) {
+    newLeft = increment.slice(0, -1);
+    newMiddle = increment.slice(-1);
+  } else {
+    newLeft = increment;
+    newMiddle = "";
+  }
+  return BigInt(mirror(newLeft, newMiddle));
+}
