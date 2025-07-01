bd8a0b07f2c1763e0641111f36864568235a5ca4
Nicholas Griffiths <nichg999@gmail.com>
2025-06-30 21:38:00 -0600

    completed algo 4

--- index2.js
+++ index2.js
@@ -117,3 +117,27 @@ function nextPalin(number) {
   }
   return BigInt(mirror(newLeft, newMiddle));
 }
+
+// Algo 4
+
+// Write a function that will take in any array and reverse it.
+
+// Sounds simple doesn't it?
+
+// NOTES:
+
+// Array should be reversed in place! (no need to return it)
+// Usual builtins have been deactivated. Don't count on them.
+// You'll have to do it fast enough, so think about performances
+
+function reverse(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    let temporary = arr[left];
+    arr[left] = arr[right];
+    arr[right] = temporary;
+    left++;
+    right--;
+  }
+}
