ae2e1b1f9bfda7854331d636aa0d3b247761d236
Nicholas Griffiths <nichg999@gmail.com>
2025-07-01 22:56:25 -0600

    finished algo 5

--- index2.js
+++ index2.js
@@ -141,3 +141,26 @@ function reverse(arr) {
     right--;
   }
 }
+
+// Algo 5
+
+// Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case). The next words should be always capitalized.
+
+// Examples
+// "the-stealth-warrior" gets converted to "theStealthWarrior"
+
+// "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+
+// "The_Stealth-Warrior" gets converted to "TheStealthWarrior"
+
+function toCamelCase(str) {
+  let words = str.split(/[-_]/);
+  return words
+    .map((word, index) => {
+    if (index === 0) {
+      return word;
+    }
+    return word.charAt(0).toUpperCase() + word.slice(1);
+    })
+    .join("");
+}
