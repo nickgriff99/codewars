57812ab0a13c87074866f2ae6bc080d9d1b60578
Nicholas Griffiths <nichg999@gmail.com>
2025-07-02 19:34:19 -0600

    last commit wasn't a 6kyu, found a new algo and completed it

--- index2.js
+++ index2.js
@@ -167,24 +167,29 @@ function toCamelCase(str) {
 
 // Algo 6
 
-// Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+// Your task is to construct a building which will be a pile of n cubes. The cube at the bottom will have a volume of 
+// n^3, the cube above will have volume of (n - 1)^3 and so on until the top which will have a volume of 1^3
 
-// Examples
-// pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
-// pigIt('Hello world !');     // elloHay orldway !
+// You are given the total volume m of the building. Being given m can you find the number n of cubes you will have to build?
 
-function pigIt(str) {
-  const pig = str.split(" ");
-  const pigLat = pig.map(word => {
-    if (/^[!?.;:,]$/.test(word)) {
-      return word;
+// The parameter of the function findNb (find_nb, find-nb, findNb, ...) will be an integer m and you have to return the integer n such as
+// n^3 + (n - 1)^3 + (n - 2)^3 + ... + 1^3 = m if such an n exists or -1 if there is no such n.
+
+// examples: 
+
+// findNb(1071225) --> 45
+
+// findNb(91716553919377) --> -1
+
+function findNb(m) {
+  let sum = 0;
+  let n = 0;
+  while (sum < m) {
+    n += 1;
+    sum += Math.pow(n, 3);
+    if (sum === m) {
+      return n;
     }
-    if (word.length > 0) {
-      const firstLetter = word[0];
-      const restOfWord = word.slice(1);
-      return restOfWord + firstLetter + "ay";
-    }
-    return word;
-  })
-  return pigLat.join(" ");
+  }
+  return -1;
 }
