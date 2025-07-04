b3d216e557cd50bb791164c0acc6bdea988f2eac
Nicholas Griffiths <nichg999@gmail.com>
2025-07-03 20:46:13 -0600

    finished algo 8

--- index2.js
+++ index2.js
@@ -216,3 +216,20 @@ function isPrime(num) {
   }
   return true;
 }
+
+// Algo 8
+
+// Write a method (or function, depending on the language) that converts a string to camelCase, that is, all words must have their first letter capitalized and spaces must be removed.
+
+// Examples (input --> output):
+// "hello case" --> "HelloCase"
+// "camel case word" --> "CamelCaseWord"
+// Don't forget to rate this kata! Thanks :)
+
+String.prototype.camelCased = function() {
+  return this.trim()
+    .split(/\s+/)
+    .filter(Boolean)
+    .map(word => word[0].toUpperCase() + word.slice(1))
+    .join("");
+}
