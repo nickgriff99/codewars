c2d35a7df9b98a4c57a1176018c4a88d6a98666e
Nicholas Griffiths <nichg999@gmail.com>
2025-07-05 21:28:19 -0600

    finished algo 10

--- index2.js
+++ index2.js
@@ -264,4 +264,40 @@ function validPhoneNumber(phoneNumber) {
   return true;
 }
 
-console.log(validPhoneNumber("(123) 456-7890"))
+// Algo 10
+
+// Given an array of integers of any length, return an array that has 1 added to the value represented by the array.
+
+// If the array is invalid (empty, or contains negative integers or integers with more than 1 digit), return nil (or your language's equivalent).
+
+// Examples
+// Valid arrays
+
+// [4, 3, 2, 5] would return [4, 3, 2, 6] (4325 + 1 = 4326)
+// [1, 2, 3, 9] would return [1, 2, 4, 0] (1239 + 1 = 1240)
+// [9, 9, 9, 9] would return [1, 0, 0, 0, 0] (9999 + 1 = 10000)
+// [0, 1, 3, 7] would return [0, 1, 3, 8] (0137 + 1 = 0138)
+// Invalid arrays
+
+// [] is invalid because it is empty
+// [1, -9] is invalid because -9 is not a non-negative integer
+// [1, 2, 33] is invalid because 33 is not a single-digit integer
+
+function upArray(arr) {
+  if (arr.length === 0) return null;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < 0 || arr[i] > 9) {
+      return null;
+    }
+  }
+  let result = arr.slice();
+  for (let i = result.length - 1; i >= 0; i--) {
+    if (result[i] < 9) {
+      result[i]++;
+      return result;
+    }
+    result[i] = 0;
+  }
+  result.unshift(1);
+  return result;
+}
