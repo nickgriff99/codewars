93920d633817cf56d7c8c44fece54fcb2cba3f46
Nicholas Griffiths <nichg999@gmail.com>
2025-07-06 22:06:47 -0600

    finished algo 11

--- index2.js
+++ index2.js
@@ -301,3 +301,23 @@ function upArray(arr) {
   result.unshift(1);
   return result;
 }
+
+// Algo 11
+
+// Write a function that given an integer n >= 0, returns an array of n ascending length subarrays, all filled with 1s.
+
+// 0 => [ ]
+// 1 => [ [1] ]
+// 2 => [ [1], [1, 1] ]
+// 3 => [ [1], [1, 1], [1, 1, 1] ]
+
+function pyramid(n) {
+  const result = [];
+  for (let i = 1; i <= n; i++) {
+    result.push(Array(i).fill(1));
+  }
+  return result;
+}
+
+console.log(pyramid(2));
+
