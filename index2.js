bff4d4f45a134116173136fa69e39c5eee1b1f13
Nicholas Griffiths <nichg999@gmail.com>
2025-07-07 20:01:35 -0600

    finished algo 13

--- index2.js
+++ index2.js
@@ -343,3 +343,27 @@ function pascalsTriangle(n) {
   }
   return result.flat();
 }
+
+// Algo 13
+
+// Assume "#" is like a backspace in string. This means that string "a#bc#d" actually is "bd"
+
+// Your task is to process a string with "#" symbols.
+
+// Examples
+// "abc#d##c"      ==>  "ac"
+// "abc##d######"  ==>  ""
+// "#######"       ==>  ""
+// ""              ==>  ""
+
+function cleanString(s) {
+  const arr = [];
+  for (let char of s) {
+    if (char === "#") {
+      arr.pop();
+    } else {
+      arr.push(char);
+    }
+  }
+  return arr.join('');
+}
