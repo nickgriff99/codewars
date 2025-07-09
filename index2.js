4383d96da40e2bd8ce1555c790cc77c8b183067d
Nicholas Griffiths <nichg999@gmail.com>
2025-07-08 23:27:50 -0600

    finished algo 18

--- index2.js
+++ index2.js
@@ -497,3 +497,29 @@ function removeParenthesis(s) {
   }
   return result;
 }
+
+// ALgo 18
+
+// You are given an input string.
+
+// For each symbol in the string if it's the first character occurrence, replace it with a '1', else replace it with the amount of times you've already seen it.
+
+// Examples:
+// input   =  "Hello, World!"
+// result  =  "1112111121311"
+
+// input   =  "aaaaaaaaaaaa"
+// result  =  "123456789101112"
+// There might be some non-ascii characters in the string.
+
+// Take note of performance
+
+function numericals(s) {
+  let counts = {};
+  let result = [];
+  for (let char of s) {
+    counts[char] = (counts[char] || 0) + 1;
+    result.push(counts[char]);
+  }
+  return result.join('');
+}
