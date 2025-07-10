b44f74010e23ac4ff6fb8a194626de6806c6557b
Nicholas Griffiths <nichg999@gmail.com>
2025-07-09 23:20:10 -0600

    finished algo 19

--- index2.js
+++ index2.js
@@ -523,3 +523,22 @@ function numericals(s) {
   }
   return result.join('');
 }
+
+// Algo 19
+
+// The function 'fibonacci' should return an array of fibonacci numbers. The function takes a number as an argument to decide how many no. of elements to produce. If the argument is less than or equal to 0 then return empty array
+
+// Example:
+
+// fibonacci(4) // should return  [0,1,1,2]
+// fibonacci(-1) // should return []
+
+function fibonacci(n) {
+  if (n <= 0) return [];
+  if (n === 1) return [0];
+  let result = [0, 1];
+  while (result.length < n) {
+    result.push(result[result.length - 1] + result[result.length - 2]);
+  }
+  return result;
+}
