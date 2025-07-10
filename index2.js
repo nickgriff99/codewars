12ad2644369a75e4b1783df4dfe124d7262a74ef
Nicholas Griffiths <nichg999@gmail.com>
2025-07-09 23:30:31 -0600

    finished algo 20

--- index2.js
+++ index2.js
@@ -542,3 +542,27 @@ function fibonacci(n) {
   }
   return result;
 }
+
+// Algo 20
+
+// Complete the method so that it formats the words into a single comma separated value. The last word should be separated by the word 'and' instead of a comma. The method takes in an array of strings and returns a single formatted string.
+
+// Note:
+
+// Empty string values should be ignored.
+// Empty arrays or null/nil/None values being passed into the method should result in an empty string being returned.
+// Example: (Input --> output)
+
+// ['ninja', 'samurai', 'ronin'] --> "ninja, samurai and ronin"
+// ['ninja', '', 'ronin'] --> "ninja and ronin"
+// [] -->""
+
+function formatWords(words) {
+  return words && (words = words.filter(Boolean)).length
+    ? words.length === 1
+      ? words[0]
+      : words.slice(0, -1).join(', ') + ' and ' + words.slice(-1)
+    : '';
+}
+
+console.log(formatWords(['ninja', 'samurai', 'ronin']))
